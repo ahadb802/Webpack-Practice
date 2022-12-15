88e25572307ffcb08580ef0c3ed0dc7e79f637cb
sambeck87 <sandro_mipb@hotmail.com>
2022-12-15 09:04:42 -0600

    Add test for check the complete items Add test for delete all complete items

--- toDoList.test.js
+++ toDoList.test.js
@@ -4,6 +4,8 @@
 const addTodoTask = require('./src/modules/addTodoTask.js');
 const removeToDo = require('./src/modules/removeToDo.js');
 const updateTodo = require('./src/modules/updateToDo.js');
+const checkTodo = require('./src/modules/checkToDoList.js');
+const clearAllCompleted = require('./src/modules/clearAll.js');
 
 describe('todo functions - ADD - REMOVE - UPDATE - COMPLETED - CLEAR ALL', () => {
   test('add a todo', () => {
@@ -65,6 +67,60 @@ describe('todo functions - ADD - REMOVE - UPDATE - COMPLETED - CLEAR ALL', () =>
     const i = 0;
     const output = updateTodo(i, todoList, 'edit the discription');
     expect(output).toEqual([{ desc: 'edit the discription', completed: false, index: 0 }]);
+    localStorage.setItem('toDoListItem', JSON.stringify(output));
+    expect(JSON.parse(localStorage.getItem('toDoListItem'))).toEqual(output);
+    document.body.innerHTML = '';
+    for (let i = 0; i < output.length; i += 1) {
+      document.body.innerHTML += '<div class="list-item">'
+      + '  <ul id="list"><li></li></ul>'
+      + '</div>';
+    }
+    const list = document.querySelectorAll('.list-item');
+    expect(list).toHaveLength(output.length);
+  });
+
+  test('check for complete item', () => {
+    const todoList = [
+      {
+        desc: 'one',
+        completed: false,
+        index: 0,
+      },
+    ];
+    const i = 0;
+    const output = checkTodo(i, todoList);
+    expect(output).toEqual([{ desc: 'one', completed: true, index: 0 }]);
+    localStorage.setItem('toDoListItem', JSON.stringify(output));
+    expect(JSON.parse(localStorage.getItem('toDoListItem'))).toEqual(output);
+    document.body.innerHTML = '';
+    for (let i = 0; i < output.length; i += 1) {
+      document.body.innerHTML += '<div class="list-item">'
+      + '  <ul id="list"><li></li></ul>'
+      + '</div>';
+    }
+    const list = document.querySelectorAll('.list-item');
+    expect(list).toHaveLength(output.length);
+  });
+  test('clearing all complete item', () => {
+    const todoList = [
+      {
+        desc: 'one',
+        completed: false,
+        index: 0,
+      },
+      {
+        desc: 'two',
+        completed: true,
+        index: 1,
+      },
+      {
+        desc: 'three',
+        completed: false,
+        index: 2,
+      },
+    ];
+    const output = clearAllCompleted(todoList);
+    expect(output).toEqual([{ desc: 'one', completed: false, index: 1 }, { desc: 'three', completed: false, index: 2 }]);
     localStorage.setItem('toDoListItem', JSON.stringify(output));
     expect(JSON.parse(localStorage.getItem('toDoListItem'))).toEqual(output);
     document.body.innerHTML = '';
