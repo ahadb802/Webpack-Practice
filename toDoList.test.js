5a22ac778c73ea6ececfcbc9dc05d1e2949bac10
Sandro Hernandez <sandro_mipb@hotmail.com>
2022-12-14 18:15:00 -0600

    Add remove test function

--- toDoList.test.js
+++ toDoList.test.js
@@ -2,6 +2,7 @@
 * @jest-environment jsdom
 */
 const addTodoTask = require('./src/modules/addTodoTask.js');
+const removeToDo = require('./src/modules/removeToDo.js'); 
 
 describe('todo functions - ADD & REMOVE', () => {
   test('add a todo', () => {
@@ -24,5 +25,33 @@ describe('todo functions - ADD & REMOVE', () => {
     }
     const list = document.querySelectorAll('.list-item');
     expect(list).toHaveLength(result.length);
+  });
+  
+  test('delete an item', () => {
+    const todoList = [
+      {
+        desc: 'one',
+        completed: false,
+        index: 0,
+      },
+      {
+        desc: 'two',
+        completed: false,
+        index: 1,
+      },
+    ];
+    const i = 0;
+    const output = removeToDo(i, todoList);
+    expect(output).toEqual([{ desc: 'two', completed: false, index: 1 }]);
+    localStorage.setItem('toDoListItem', JSON.stringify(output));
+    expect(JSON.parse(localStorage.getItem('toDoListItem'))).toEqual(output);
+    document.body.innerHTML = '';
+    for (let i = 0; i < output.length; i += 1) {
+      document.body.innerHTML += '<div class="list-item">'
+      + '  <ul id="list"><li></li></ul>'
+      + '</div>';
+    }
+    const list = document.querySelectorAll('.list-item');
+    expect(list).toHaveLength(output.length);
   });
 });
